6c85dafd2f94244d8e712170424833b9eca7f993
JOSE SANCHEZ <danielsanchez524@mesoamericana.edu.gt>
2022-03-06 22:06:41 -0600

    Agregar archivos de proyecto.

---
+++ Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.Lab1DeRepaso
+{
+    internal class Empleado
+    {
+        
+
+        public int NoEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public double sueldo { get; set; }
+        public int HorasMes { get; set; }
+        public double sueldoCalculado { get; set; } 
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5.Lab1DeRepaso
+{
+    public partial class Form1 : Form
+    {
+        List<Empleado> EmpleadoList=new List<Empleado>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Leer();
+            LeerMess();
+
+            foreach (var empleado in EmpleadoList)
+            {
+                empleado.sueldoCalculado = empleado.sueldo * empleado.HorasMes;
+  
+            }
+            dataGridView1.DataSource = null;
+
+            dataGridView1.Refresh();
+            dataGridView1.DataSource = EmpleadoList;
+          
+            dataGridView1.Refresh();
+
+        }
+
+        void Leer ()
+        {
+
+            FileStream stream = new FileStream("Empleados.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Empleado empleado = new Empleado();
+
+                empleado.NoEmpleado = Convert.ToInt32(reader.ReadLine());
+                empleado.Nombre = reader.ReadLine();
+                empleado.sueldo = Convert.ToDouble(reader.ReadLine());
+                
+                EmpleadoList.Add(empleado);
+
+            }
+            reader.Close();
+
+        }
+        void LeerMess()
+        {
+
+            FileStream stream = new FileStream("Enero.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Empleado empleado = new Empleado();
+
+                string numero = reader.ReadLine();
+                string horas = reader.ReadLine();
+
+                foreach(var e in EmpleadoList )
+                {
+                    if(e.NoEmpleado ==Convert.ToInt32(numero))
+                    {
+                        e.HorasMes=Convert.ToInt32(horas);
+
+                    }
+                }
+
+            }
+            reader.Close();
+
+        }
+    }
+}
